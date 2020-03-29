342193bce0517a209510117051fe3e2d9ddd7a79
Lukas <lukas.feye@tum.de>
2020-03-29 08:56:46 +0200

    modify DQN

--- DQN.py
+++ DQN.py
@@ -5,20 +5,23 @@ import torch.nn.functional as F
 import numpy as np
 from collections import namedtuple
 import random
+from keras.models import load_model
 
 class hidden_unit(nn.Module):
     def __init__(self, in_channels, out_channels, activation):
         super(hidden_unit, self).__init__()
         self.activation = activation
-        self.nn = nn.Linear(in_channels, out_channels)
-    
+        # linear transformation to the incoming data
+        self.nn = nn.Linear(in_channels, out_channels, bias=False)
+        nn.init.normal_(self.nn.weight, std=0.07)
+
     def forward(self, x):
         out = self.nn(x)
         out = self.activation(out)   
         return out
         
 class Q_learning(nn.Module):
-    def __init__(self, in_channels, hidden_layers, out_channels, unit = hidden_unit, activation = F.relu):
+    def __init__(self, in_channels, hidden_layers, out_channels, unit=hidden_unit, activation=F.relu):
         super(Q_learning, self).__init__()
         assert type(hidden_layers) is list
         self.hidden_units = nn.ModuleList()
@@ -27,10 +30,16 @@ class Q_learning(nn.Module):
         for hidden in hidden_layers:
             self.hidden_units.append(unit(prev_layer, hidden, activation))
             prev_layer = hidden
-        self.final_unit = nn.Linear(prev_layer, out_channels)
-    
+        self.final_unit = nn.Linear(prev_layer, out_channels, bias=False)
+        nn.init.normal_(self.final_unit.weight, std=0.07)
+        model_load = load_model('/Users/Lukas/repositories/Reinforcement-Learning-Q-learning-Gridworld-Keras/simpleModel.h5')
+        weights = model_load.get_weights()
+        self.hidden_units[0].nn.weight.data = torch.from_numpy(np.transpose(weights[0]))
+        self.hidden_units[1].nn.weight.data = torch.from_numpy(np.transpose(weights[2]))
+        self.final_unit.weight.data = torch.from_numpy(np.transpose(weights[4]))
+
     def forward(self, x):
-        out = x.view(-1,self.in_channels).float()
+        out = x.view(-1, self.in_channels).float()
         for unit in self.hidden_units:
             out = unit(out)
         out = self.final_unit(out)
