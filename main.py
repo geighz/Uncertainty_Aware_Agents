7f9df25a38a1b8fbcd06658656de0a48e5709bbb
Lukas <lukas.feye@tum.de>
2020-04-02 12:54:30 +0200

    Spit the one neural network into two for each player

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ import torch
 
 ## Include the replay experience
 
-epochs = 10
+epochs = 1000
 gamma = 0.9  # since it may take several moves to goal, making gamma high
 epsilon = 1
-model = Q_learning(80, [164, 150], 16, hidden_unit)
+model_a = Q_learning(80, [164, 150], 4, hidden_unit)
+model_b = Q_learning(80, [164, 150], 4, hidden_unit)
 # for hidden in model.hidden_units:
 #     print(hidden.nn.weight.size())
 #     print(hidden.nn.weight)
 # print(model.final_unit.weight.size())
 # print(model.final_unit.weight)
-optimizer = optim.RMSprop(model.parameters(), lr=0.001)
+optimizer_a = optim.RMSprop(model_a.parameters(), lr=0.001)
+optimizer_b = optim.RMSprop(model_b.parameters(), lr=0.001)
 # optimizer = optim.SGD(model.parameters(), lr=0.1)
-loss = torch.nn.MSELoss()
+loss_a = torch.nn.MSELoss()
+loss_b = torch.nn.MSELoss()
 # buffer = 80
 # BATCH_SIZE = 10
 # memory = ReplayMemory(buffer)
@@ -31,17 +34,23 @@ for i in range(epochs):
     # while game still in progress
     while not game_over:
         v_state = Variable(torch.from_numpy(state)).view(1, -1)
-        qval = model(v_state)
-        print(qval)
+        qval_a = model_a(v_state)
+        qval_b = model_b(v_state)
+        print(qval_a)
+        print(qval_b)
         if np.random.random() < epsilon:  # choose random action
-            action = np.random.randint(0, 16)
-            print("Take random action {}".format(action))
+            action_a = np.random.randint(0, 4)
+            print("A takes random action {}".format(action_a))
         else:  # choose best action from Q(s,a) values
-            action = np.argmax(qval.data)
-            print("Take best action {}".format(action))
+            action_a = np.argmax(qval_a.data)
+            print("A takes best action {}".format(action_a))
+        if np.random.random() < epsilon:  # choose random action
+            action_b = np.random.randint(0, 4)
+            print("B takes random action {}".format(action_b))
+        else:  # choose best action from Q(s,a) values
+            action_b = np.argmax(qval_b.data)
+            print("B takes best action {}".format(action_b))
         # Take action, observe new state S'
-        action_a = action // 4
-        action_b = action % 4
         new_state = make_move(state, action_a, action_b)
         # Observe reward
         reward = get_reward(new_state)
@@ -50,32 +59,47 @@ for i in range(epochs):
         print("\n")
         step += 1
         v_new_state = Variable(torch.from_numpy(new_state)).view(1, -1)
-        newQ = model(v_new_state)
-        maxQ = newQ.max(1)[0].detach()
-        target = qval.clone().detach()
-        if reward == -1:  # non-terminal state
-            update = (reward + (gamma * maxQ))
+        newQ_a = model_a(v_new_state)
+        maxQ_a = newQ_a.max(1)[0].detach()
+        newQ_b = model_b(v_new_state)
+        maxQ_b = newQ_b.max(1)[0].detach()
+        target_a = qval_a.clone().detach()
+        target_b = qval_b.clone().detach()
+        if reward == -2:  # non-terminal state
+            update_a = (reward + (gamma * maxQ_a))
+            update_b = (reward + (gamma * maxQ_b))
         else:  # terminal state
-            update = reward
-        target[0][action] = update  # target output
-        print("Adjust\n{}\ntowards\n{}".format(qval, target))
+            update_a = reward
+            update_b = reward
+        target_a[0][action_a] = update_a  # target output
+        target_b[0][action_b] = update_b  # target output
+        print("A: Adjust\n{}\ntowards\n{}".format(qval_a, target_a))
+        print("B: Adjust\n{}\ntowards\n{}".format(qval_b, target_b))
         # dies sorgt für einen backward pass nicht nur durch qval sondern auch durch target
-        output = loss(qval, target)
+        output_a = loss_a(qval_a, target_a)
+        output_b = loss_b(qval_b, target_b)
 
         # Optimize the model
         # Clear gradients of all optimized torch.Tensor s.
-        optimizer.zero_grad()
+        optimizer_a.zero_grad()
+        optimizer_b.zero_grad()
         # compute gradients
-        output.backward()
+        output_a.backward()
+        output_b.backward()
         # Gradient clipping can keep things stable.
         # for p in model.parameters():
         #     p.grad.data.clamp_(-1, 1)
         # update model parameters
-        optimizer.step()
-        newqval = model(v_state)
-        print("New Qval\n{}".format(newqval))
-        difference = qval - newqval
-        print(difference)
+        optimizer_a.step()
+        optimizer_b.step()
+        newqval_a = model_a(v_state)
+        newqval_b = model_b(v_state)
+        print("New Qval\n{}".format(newqval_a))
+        print("New Qval\n{}".format(newqval_b))
+        difference_a = qval_a - newqval_a
+        difference_b = qval_b - newqval_b
+        print(difference_a)
+        print(difference_b)
         print()
 
         state = new_state
@@ -103,21 +127,23 @@ def testAlgo(init=0):
     # while game still in progress
     while (status == 1):
         v_state = Variable(torch.from_numpy(state))
-        qval = model(v_state.view(80))
-        print(qval)
-        action = np.argmax(qval.data)  # take action with highest Q-value
-        print('Move #: %s; Taking action: %s' % (i, action))
-        action_a = action // 4
-        action_b = action % 4
+        qval_a = model_a(v_state.view(80))
+        qval_b = model_b(v_state.view(80))
+        print(qval_a)
+        print(qval_b)
+        action_a = np.argmax(qval_a.data)  # take action with highest Q-value
+        action_b = np.argmax(qval_b.data)  # take action with highest Q-value
+        print('A: Move #: %s; Taking action: %s' % (i, action_a))
+        print('B: Move #: %s; Taking action: %s' % (i, action_b))
         state = make_move(state, action_a, action_b)
         print(disp_grid(state))
         reward = get_reward(state)
-        if reward != -1:
+        if reward != -2:
             status = 0
             print("Reward: %s" % (reward,))
         i += 1  # If we're taking more than 10 actions, just stop, we probably can't win this game
         if i > 10:
-            print("Game lost; too many moves.")
+            print("Reward: Game lost; too many moves.")
             break
 
 
