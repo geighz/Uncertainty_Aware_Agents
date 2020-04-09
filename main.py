8a9e313a53083248877a64e334efa295329570f4
Lukas <lukas.feye@tum.de>
2020-04-09 18:12:20 +0200

    fix bug where agents couldnt learn an optimal policy

--- main.py
+++ main.py
@@ -81,7 +81,7 @@ agent_b.set_partner(agent_a)
 
 # TODO can I put the optimizer into the Miner class
 optimizer_a = optim.Adam(agent_a.get_model_parameters(), lr=0.001)
-optimizer_b = optim.Adam(agent_a.get_model_parameters(), lr=0.001)
+optimizer_b = optim.Adam(agent_b.get_model_parameters(), lr=0.001)
 # optimizer_a = optim.SGD(model_a.parameters(), lr=0.02)
 # optimizer_b = optim.SGD(model_b.parameters(), lr=0.02)
 criterion_a = torch.nn.MSELoss()
