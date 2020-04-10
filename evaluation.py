41fd23be6f19be289dc56ebecf5e32d429a15b87
Lukas <lukas.feye@tum.de>
2020-04-10 18:56:58 +0200

    rename observation variable to state

--- evaluation.py
+++ evaluation.py
@@ -6,14 +6,14 @@ def evaluate_agents(agent_a, agent_b):
     reward_sum = 0
     env = Goldmine()
     for state_id in range(50):
-        observation = env.reset(state_id)
+        state = env.reset(state_id)
         # env.render()
         steps = 0
         done = False
         while not done:
-            action_a = agent_a.choose_best_action(observation)
-            action_b = agent_b.choose_best_action(observation)
-            observation, reward, done, info = env.step(action_a, action_b)
+            action_a = agent_a.choose_best_action(state)
+            action_b = agent_b.choose_best_action(state)
+            state, reward, done, _ = env.step(action_a, action_b)
             # env.render()
             reward_sum += reward
             steps += 1
