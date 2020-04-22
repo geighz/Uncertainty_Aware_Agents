69e6ace969e294febd96c58a2534598084445fa1
Lukas <lukas.feye@tum.de>
2020-04-22 19:15:08 +0200

    -name a constant

--- evaluation.py
+++ evaluation.py
@@ -3,11 +3,14 @@ from gridworld import Goldmine
 from torch.autograd import Variable
 import torch
 
+total_number_of_eval_games = 50
+
 
 def evaluate_agents(agent_a, agent_b):
     reward_sum = 0
+    # TODO: do we load the sates multiple times from disk?
     env = Goldmine()
-    for state_id in range(50):
+    for state_id in range(total_number_of_eval_games):
         state = env.reset(state_id)
         # env.render()
         steps = 0
@@ -21,4 +24,4 @@ def evaluate_agents(agent_a, agent_b):
             steps += 1
             if steps > 10:
                 done = True
-    return reward_sum / 50
+    return reward_sum / total_number_of_eval_games
