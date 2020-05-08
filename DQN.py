7a9489f7532faf923e85448577e9100af68a7522
Lukas <lukas.feye@tum.de>
2020-05-08 07:58:36 +0200

    -prettify code -rename class from DQN_from_new to Head_net -enable to use multiple last layer as heads

--- DQN.py
+++ DQN.py
@@ -24,18 +24,15 @@ class Body_net(nn.Module):
         self.in_channels = in_channels
         self.activation = activation
         prev_layer = in_channels
+        hidden_layers = hidden_layers + [out_channels]
         for hidden in hidden_layers:
             self.hidden_units.append(unit(prev_layer, hidden, activation))
             prev_layer = hidden
-        self.final_unit = nn.Linear(prev_layer, out_channels)
-        nn.init.normal_(self.final_unit.weight, std=0.07)
 
     def forward(self, x):
         out = x.view(-1, self.in_channels).float()
         for unit in self.hidden_units:
             out = unit(out)
-        out = self.final_unit(out)
-        out = self.activation(out)
         return out
 
 
@@ -43,7 +40,9 @@ class DQN_from_net(nn.Module):
     def __init__(self, net, out_channels):
         super(DQN_from_net, self).__init__()
         self.net = net
-        self.final_unit = nn.Linear(self.net.final_unit.out_features, out_channels)
+        self.final_units = nn.ModuleList()
+        last_index = len(self.net.hidden_units) - 1
+        self.in_channels = self.net.hidden_units[last_index].nn.out_features
         nn.init.normal_(self.final_unit.weight, std=0.07)
 
     def forward(self, x):
