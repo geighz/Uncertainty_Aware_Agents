01e250dd95c1f5cb6b00f432206989a13cd36018
Lukas <lukas.feye@tum.de>
2020-06-25 15:50:11 +0200

    no message

--- main.py
+++ main.py
@@ -4,37 +4,49 @@ from UncertaintyAwareMiner import UncertaintyAwareMiner
 from UncertaintyAwareMinerNormalised import UncertaintyAwareMinerNormalised
 from TestExecutor import *
 from Plotter import *
-import multiprocessing
+from multiprocessing import Pool, cpu_count, Manager
+import psutil
+import os
 import time
 
 start_time = time.time()
-EPOCHS = 1001
+EPOCHS = 501
 BUFFER = 80
 BATCH_SIZE = 10
 TARGET_UPDATE = 5
-NUMBER_EXECUTIONS = 1
+NUMBER_EXECUTIONS = 3
 BUDGET = 250
 
 test_setups = [
     # Test_setup(NoAdviceMiner, 1, EPOCHS, BUFFER, BATCH_SIZE, TARGET_UPDATE, BUDGET),
-    Test_setup(VisitBasedMiner, 1, EPOCHS, BUFFER, BATCH_SIZE, TARGET_UPDATE, BUDGET),
-    Test_setup(UncertaintyAwareMiner, 5, EPOCHS, BUFFER, BATCH_SIZE, TARGET_UPDATE, BUDGET),
+    # Test_setup(VisitBasedMiner, 1, EPOCHS, BUFFER, BATCH_SIZE, TARGET_UPDATE, BUDGET),
+    # Test_setup(UncertaintyAwareMiner, 5, EPOCHS, BUFFER, BATCH_SIZE, TARGET_UPDATE, BUDGET),
     Test_setup(UncertaintyAwareMinerNormalised, 5, EPOCHS, BUFFER, BATCH_SIZE, TARGET_UPDATE, BUDGET)
 ]
 
-manager = multiprocessing.Manager()
+manager = Manager()
 test_results = manager.dict()
 
 # for test_setup in test_setups:
 #     for test_number in range(NUMBER_EXECUTIONS):
 #         execute_test(id, test_setup, test_results)
 
+
+def limit_cpu():
+    p = psutil.Process(os.getpid())
+    # second lowest priority
+    p.nice(19)
+
+
 testProcesses = []
 id = 0
+print(psutil.cpu_count(logical=False))
+print(psutil.cpu_count(logical=True))
+pool = Pool(processes=12, initializer=limit_cpu())
 for test_setup in test_setups:
     for test_number in range(NUMBER_EXECUTIONS):
         id += 1
-        testProcess = multiprocessing.Process(target=execute_test, args=(id, test_setup, test_results))
+        testProcess = pool.Process(target=execute_test, args=(id, test_setup, test_results))
         testProcesses.append(testProcess)
         testProcess.start()
 
