25a05b749c96fc086f24121fe50a4e17b9a369b4
geighz <geighz@yahoo.com>
2023-08-13 19:20:39 -0400

    batches courage

---
+++ batches_courage.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Make executable chmod +x batches_courage.sh
+# run: bash batches_courage.sh
+# python3 main.py
+# python3 main.py
+python3 TuneHyper.py
+python3 TuneHyper1.py
+python3 TuneHyper2.py
+
